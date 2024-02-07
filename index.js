edf6c95b2babb1eddfdd62e9efe8abc33257c46c
MathewIsBack <oseghaleokononfua@gmail.com>
2024-02-07 13:13:20 +0100

    user model created

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 dotenv.config();
+const port = process.env.PORT || 5000
+const corsOptions = {
+    origin: true,
+    credentials: true
+}
+
+const app = express();
+
+mongoose.set('strictQuery', false);
+const connectDb = async () => {
+    try{
+        await mongoose.connect(process.env.MONGODBURI);
+        console.log('MongoDB database connected')
+    }catch(err){
+        console.log('MongoDb database connection failed')
+    }
+}
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.listen(port, () => {
+    connectDb();
+    console.log(`app is listening on port ${port}`)
+})
---
+++ models/User.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    avatar: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/previews/020/765/399/non_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg"
+    },
+},
+    { timestamps: true }
+)
+
+export default mongoose.model('User', userSchema)
